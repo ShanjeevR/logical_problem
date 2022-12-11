8f73343a73452f6484f473a1f43a165d407e1934
ShanjeevR <shanjeev.kar98@gmail.com>
2022-12-11 11:22:18 +0530

    a=111

--- prime_no.py
+++ prime_no.py
@@ -1,9 +1,9 @@
-a=55
+a=5
 sum=0
 for i in range(2,a//2):
     if a%i==0:
         sum=sum+1
 if sum==0:
-    print("Its a prime number")
+    print("prime number")
 else:
     print("Not a prime number")            
